f6910c2bc5959495fd779eff4577871e3ed8eeb9
Daniel <da_919@hotmail.com>
2023-09-07 18:52:20 -0500

    se eliminan imports innesesarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
-
+import { Module } from "@nestjs/common";
+import { BooksModule } from "./books/books.module";
 @Module({
   imports: [BooksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
