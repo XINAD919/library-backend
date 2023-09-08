4215fab9c1b15c3dbd73e36b8e8ea4db9d14d48f
Daniel <da_919@hotmail.com>
2023-09-08 12:22:44 -0500

    se agrega la propiedad api de swagger

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsString,
   IsNumber,
@@ -6,34 +7,40 @@ import {
   Max,
   IsNotEmpty,
 } from "class-validator";
-
 export class CreateBookDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   author: string;
 
+  @ApiProperty({ required: false })
   @IsNumber({ maxDecimalPlaces: 0 })
   @Min(1000)
   @Max(2024)
   @IsOptional()
   publicationDate?: number;
 
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
   synopsis?: string;
 
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
   publisher?: string;
 
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
   genre?: string;
 
+  @ApiProperty({ required: false })
   @IsNumber()
   @IsOptional()
   quantity?: number;
