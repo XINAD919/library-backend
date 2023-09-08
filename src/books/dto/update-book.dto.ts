4d7710116f1e48668012025fb0c6f0d642c55365
Daniel <da_919@hotmail.com>
2023-09-07 23:36:19 -0500

    se haace el dto de update

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,6 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBookDto } from './create-book.dto';
-
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateBookDto } from "./create-book.dto";
+import { IsString, IsNumber, IsOptional, Min, Max } from "class-validator";
 export class UpdateBookDto extends PartialType(CreateBookDto) {
+  @IsString()
+  @IsOptional()
+  title?: string;
 
+  @IsString()
+  @IsOptional()
+  author?: string;
+
+  @IsNumber({ maxDecimalPlaces: 0 })
+  @Min(1000)
+  @Max(2024)
+  @IsOptional()
+  publicationDate?: number;
+
+  @IsString()
+  @IsOptional()
+  synopsis?: string;
+
+  @IsString()
+  @IsOptional()
+  publisher?: string;
+
+  @IsString()
+  @IsOptional()
+  genre?: string;
+
+  @IsNumber()
+  @IsOptional()
+  quantity?: number;
 }
